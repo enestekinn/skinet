6d916e6d9a266d7c39d701a50b16b44339246fb3
enestekin <enestekin_@hotmail.com>
2022-05-18 02:12:52 -0700

    extending the products entity and creating related entites

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,23 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
+/*
+        BaseEntity class i olusturduk bi sayede  Id ye gerek kalmadi. 
+        Fakat sirf bu yuzden onu eklemedik.Generic icin ekledik
+ */
+       // public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        //Related Entity
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+                //Related Entity
+        public ProductBrand ProductBtrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
+// Migration yapildiginda Product in ProductType ile ProductBrand ile 
+// iliskisi gozukecektir.
