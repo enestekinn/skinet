877794805f63c32eb236659853f4575860836cdd
enestekin <enestekin_@hotmail.com>
2022-05-23 12:50:38 +0300

    End of section 7

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+// Buradan deger veriyoruz bu degeri  html den cekiyoruz.
+export class AppComponent {
+  title = 'Skinet';
+}
